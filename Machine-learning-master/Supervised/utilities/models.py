51d2d9ca8121547d674993309b5dc23ec1e4981e
Mahdi Zare <thisismahdizare@gmail.com>
2021-01-09 19:37:42 -0800

    added penalty

--- Machine-learning-master/Supervised/utilities/models.py
+++ Machine-learning-master/Supervised/utilities/models.py
@@ -4,13 +4,13 @@ import errors as e
 import preprocessing as prc
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 #########################################################################################################
 
 
-class SupervisedModel():
+class LinearLogisticRegression():
     '''
-    the class "supervised model" supports some supervised learning model algorithms, such as
-    hypothesis,cost and optimzation.
+    the class "LinearLogisticRegression" provides some algorithms for both linear and logistic regression.
     
     parameters
     ----------
@@ -18,21 +18,29 @@ class SupervisedModel():
         feature matrix
     y : ndarray
         target matrix
-    w0 : ndarray
+    w : ndarray
         initial weight matrix
     m : int
         an integer number which is usually length of the given data
-    tag : str , optional
+    kind : str , optional
         order : {LinearRegression , LogisticRegression}
         to determine the problem type
-    standardize : boolean , optional
+    standard : boolean , optional
         if true, the parameter "x" will get these new features : (mean = 0 , std = 1)
+    penalty : str , optional
+        order : {'L1','L2'}
+        regularization technique.
+    alpha : float , optional
+        regularization hyper parameter.
     '''
-    def __init__(self, x, y, w0,m=None, tag='LinearRegression',standard=False,bias = True):
+    def __init__(self, x, y, w=None, bias = 0, m=None, kind='LinearRegression',standard=False,bias_included=False,penalty='L2',alpha = 0):
 
-        e.NumpyErrorCheck(x, y, w0)
+        e.NumpyErrorCheck(x, y, w)
         ##########################
-        (self.m , self.n) = x.shape
+        if x.ndim == 1:
+            x = x[:,np.newaxis]
+        (self.m,self.n)=x.shape
+
         if m is None:
             m = self.m
         if type(m) != int:
@@ -41,39 +49,66 @@ class SupervisedModel():
             else:
                 raise TypeError(''' m must be an integer.''')
         ##########################
-        if tag != 'LinearRegression' :
-            if tag != 'LogisticRegression':
-                raise ValueError(tag)
-        ##########################    
+        if kind != 'LinearRegression' :
+            if kind != 'LogisticRegression':
+                raise ValueError(kind)
+        ########################### 
+    
         self.x = x
+        self.y = y
+        self.m = m
+        self.bias = bias
+        self.penalty = penalty
+        self.alpha = alpha
+        self.kind_ = kind
+
+        if w is not None:
+            self.w = w
+        else:
+            self.w = np.ones((self.n,1))        
+        if bias_included:
+            self.bias = w[0]
+            self.w = w[1:]
         if standard:
             self.x = prc.standardization(x)
-        if bias:
-            self.x = prc.add_bias_coef(self.x)
-        self.y = y
-        self.w0 = w0
-        self.m = m
-        self.tag = tag
         ##########################
     
-    def __algs_init__(self,xyw):
-        if len(xyw)>3:
-            raise ValueError('At most 3 values to unpack')
-        if 'x' in xyw:
-            x = xyw['x']
+    def __init_algs__(self,kwg):
+        if len(kwg)>4:
+            raise ValueError('At most 4 values to unpack')
+        if 'x' in kwg:
+            x = kwg['x']
         else:
             x = self.x
-        if 'y' in xyw:
-            y = xyw['y']
+
+        if 'y' in kwg:
+            y = kwg['y']
         else:
             y = self.y
-        if 'w0' in xyw:
-            w0  = xyw['w0']
+
+        if 'w' in kwg:
+            w  = kwg['w']
         else:
-            w0  = self.w0
-        return x,y,w0
-    
-    def Hyp(self, degree = 1 , **kwargs):
+            w  = self.w
+
+        if 'bias' in kwg:
+            bias = kwg['bias']
+        else:
+            bias = self.bias
+
+        return x,y,w,bias
+
+    def _penalty(self,w):
+
+        if self.penalty =='L2':
+            #return self.alpha * np.sum(np.square(w))
+            return self.alpha * (w.T @ w)[0,0]
+        elif self.penalty =='L1':
+            return self.alpha * np.sum(np.abs(w))
+        else:
+            raise ValueError(f'''invalid value "{self.penalty}"''')
+
+    def predict(self, degree = 1, interaction=True, random_weight = False, **kwargs):
         '''
         hypothesis function for supervised models.
 
@@ -84,8 +119,7 @@ class SupervisedModel():
             use degree 2 or more for polynomial regression. (the default is 1 )
         kwargs : dictionary, optional
             it is for passing other arguments, beside self arguments.
-            names {x , y , w0} are only accepted.
-
+            names {x , y , w} are only accepted.
 
         Returns
         -------
@@ -95,21 +129,27 @@ class SupervisedModel():
         # set conditions
 
         #######################    
-        x,_,w0 = self.__algs_init__( kwargs)
-
-        if self.tag == 'LinearRegression':
+        x,_,w,bias = self.__init_algs__( kwargs)
+        poly = PolynomialFeatures(degree=degree,include_bias=False)
+        if self.kind_ == 'LinearRegression':
             def activeF(x): return x
-        elif self.tag == 'LogisticRegression':
+        elif self.kind_ == 'LogisticRegression':
             def activeF(x): return 1 / (1 + np.exp(-x))
         else:
             raise e.ActiveFuncError(
                 'Ativation function can not be activated for this problem')
         # starting the algorithm
-        hyps = np.zeros((x.shape[0], w0.shape[1]))
-        for deg in range(1, degree+1):
-            hyp = activeF(np.dot(np.power(x, deg) , w0))
-            hyps += hyp
-        return hyps
+        if interaction:
+            x = poly.fit_transform(x)
+            if random_weight:
+                w = np.random.randn(x.shape[-1],1)
+            return activeF(np.dot(x,w)+bias)
+        else:
+            hyps = np.zeros((x.shape[0], w.shape[1]))
+            for deg in range(1, degree+1):
+                hyp = activeF(np.dot(np.power(x, deg) , w)+bias)
+                hyps += hyp
+            return hyps
 
     def loss(self, A='MSE',**kwargs):
         '''
@@ -122,7 +162,7 @@ class SupervisedModel():
             to select the solving method
         kwargs : dictionary, optional
             it is for passing other arguments, beside self arguments.
-            names {x , y , w0} are only accepted.
+            names {x , y , w} are only accepted.
 
         Returns
         -------
@@ -130,23 +170,23 @@ class SupervisedModel():
         '''
         # starting the algorithm
         
-        x,y,w0 = self.__algs_init__(kwargs)
+        x,y,w,bias = self.__init_algs__(kwargs)
 
             
         if A == 'MSE':
-            def loss(H, m, y): return 1/m * np.sum(np.square((H - y)))
+            def loss(p, m, y): return 1/m * (np.sum(np.square(p - y)) + self._penalty(w))
+        elif A == 'VEC_MSE':
+            def loss(H, m, y): return 1/m * ( ((H - y).T @ (H - y))[0, 0] + self._penalty(w) )
         elif A == 'MLE':
-            def loss(H, m, y): return 1/m * np.sum(  (-y * np.log(H))    -    ((1-y)*np.log(1-H))  )
-        elif A == 'VEC':
-            def loss(H, m, y): return 1/m * (((H - y).T @ (H - y))[0, 0])
+            def loss(H, m, y): return 1/m * (np.sum( (-y * np.log(H)) - ((1-y)*np.log(1-H)) ) + self._penalty(w)) 
         else:
             raise ValueError(
                         'invalid value for parameter %s' % 'A')
-        return loss(self.Hyp(x=x,w0=w0), m= self.m,y = y)
+        return loss(self.predict(x=x,w=w,bias = bias), m= self.m,y = y)
 
 
 
-    def gradient_descent(self,los='MSE', lr=.001, maxIter=100, converLim=0.001,verbose=0 ,Type='batch', plot_j=False,inplace=False,just_fit = False,**kwargs):
+    def gradient_descent(self,los='MSE', lr=.001, maxIter=100, converLim=0.001,verbose=0 ,Type='batch', plot_j=False,inplace=False,**kwargs):
         '''
         Gradient descent algorithm .
 
@@ -182,7 +222,7 @@ class SupervisedModel():
             inplace = True
         kwargs : dictionary, optional
             it is for passing other arguments, beside self arguments.
-            names {x , y , w0} are only accepted.
+            names {x , y , w} are only accepted.
 
         Returns
         -------
@@ -192,7 +232,7 @@ class SupervisedModel():
         # variables
         costs = []
         iterNum = 0
-        x,y,w0 = self.__algs_init__(kwargs)
+        x,y,w,bias = self.__init_algs__(kwargs)
         ################
         
         # conditions
@@ -211,7 +251,8 @@ class SupervisedModel():
             raise ValueError('verbose must be smaller than maxIter')
 
         # static codes
-        def grad(Ob,x,y): return (1/Ob.m) * (np.dot(x.T, (Ob.Hyp(x =x ,w0 = w0) - y)))
+        def grad(Ob,x,y,W,B): return (1/Ob.m) * (np.dot(x.T, (Ob.predict(x =x ,w = W,bias = B) - y)))
+        def bias_grad(Ob,x,y,W,B) : return (1/Ob.m) * np.sum(Ob.predict(x=x,w=W,bias = B) - y) 
         def choose_random_values(x, y, idx): return (x[idx], y[idx])
 
         # starting the algorithm
@@ -219,17 +260,28 @@ class SupervisedModel():
 
             if Type != 'batch':
                 random_x, random_y = choose_random_values(x= x, y=y, idx=idx)
-                w0 = w0 - (lr * grad(self,random_x,random_y))
-                costs.append(cost := self.loss(A=los,x=random_x,y=random_y,w0=w0))
+                G = grad(self,random_x,random_y,w,bias)
+                B_G = bias_grad(self,random_x,random_y,w,bias)
+                bias = bias - (lr * B_G)
+                #w = w * (1 - (self.alpha * lr))  - (lr * grad(self,random_x,random_y))
+                w = w - (lr * (G + (self.alpha * w)))
+                costs.append(cost := self.loss(A=los,x=random_x,y=random_y,w=w,bias = bias))
+
             else:
-                w0 = w0 - (lr * grad(self,x,y))
-                costs.append(cost := self.loss(A=los,x=x,y=y,w0=w0))
+                G = grad(self, x, y, w, bias)
+                B_G = bias_grad(self,x,y,w,bias)
+                bias = bias - (lr * B_G)
+                #w = w * (1 - (self.alpha * lr)) - (lr * grad(self,x,y))
+                w = w - lr * (G + (self.alpha * w))
+                costs.append(cost := self.loss(A=los,x=x,y=y,w=w,bias = bias ))
+            
             iterNum += 1
             
-            for verb in range(verbose):
+            if verbose>0:
                 print('\n\niteration no. {}\nbias : {}\ncoefficients : {}\ncost : {}\n'.format(
-                    iterNum, w0[0], w0[1:], float(cost)))
-                break
+                    iterNum, bias, w, float(cost)))
+                verbose -=1
+                print(len(costs))
             ##########################################################
             try:
                 if abs(costs[-1] - costs[-2]) < converLim:
@@ -238,6 +290,7 @@ class SupervisedModel():
                     break
             except IndexError:
                 pass
+
             ##########################################################
         if plot_j:
             plt.scatter(range(1,iterNum+1),costs, color='red')
@@ -245,45 +298,16 @@ class SupervisedModel():
             plt.ylabel('cost')
             plt.show()
 
-        self.intercept_ = w0[0].reshape(1,-1)
-        self.coef_ = w0[1:].reshape(-1, 1)
         if inplace:
-            self.w0 = w0
-        if not just_fit:
-            return w0, costs[-1]
+            self.bias = w[0].reshape(1,-1)
+            self.w = w[1:].reshape(-1, 1)
 
-    def normal_equation(self,x=None,y=None,w0 = None):
+        return bias,w,costs[-1]
+
+    def normal_equation(self,**kwargs):
         '''
         normal equation for linear models.
+        note = this implentation is an old version and not updated yet.
         '''
-        if x is not None:
-            x = prc.add_bias_coef(x)
-        else:
-            x = self.x
-        ################
-        if y is not None:
-            y = y
-        else:
-            y = self.y
-        #################
-        if w0 is not None:
-            w0 = w0
-        else:
-            w0 = self.w0
+        x,y,_,_ = self.__init_algs__(kwargs)
         return (np.linalg.pinv(x.T @ x) @ x.T @ y)
-
-    def scores(self,pred , y,*args):
-        '''
-        docstring will be added later
-        '''
-        import sklearn.metrics as slm
-        if 'r2_score' in args:
-            print(f' r2_score is : {slm.r2_score(pred,y)}')
-        if 'accuracy_score' in args:
-            print(f' accuracy_score is : {slm.accuracy_score(pred,y)}')
-        if 'precision_score' in args:
-            print(f' precision_score is : {slm.precision_score(pred,y)}')
-        if 'recall_score' in args:
-            print(f' recall_score is : {slm.recall_score(pred,y)}')
-        if 'f1_score' in args:
-            print(f' r2_score is : {slm.f1_score(pred,y)}')
